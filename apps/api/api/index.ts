c7239cdd452f3256e6503c812e61339a89f4f28b
Saba Siddique <sabasiddique001@gmail.com>
2025-12-30 13:15:27 +0500

    Fix Vercel serverless function structure - add api directory

---
+++ apps/api/api/index.ts
@@ -0,0 +1,7 @@
+// Vercel serverless function wrapper
+// This file is automatically detected by Vercel as a serverless function
+// Vercel will compile TypeScript, so we can import from source
+import handler from "../src/server"
+
+export default handler
+
